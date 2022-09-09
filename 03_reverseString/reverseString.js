b3856a90a896cab53cae809e0a822edb9c97a585
BellaTovm <tovmasyanmd@gmail.com>
2022-09-09 12:51:19 +0100

    reverse

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    let gago = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        gago += str[i];
+    }
+    return gago;
 };
 
 // Do not edit below this line
