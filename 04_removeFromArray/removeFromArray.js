5c1c65b95d71c55557fb3d2292f2e3afc2d90ee6
BellaTovm <tovmasyanmd@gmail.com>
2022-09-11 12:26:28 +0100

    removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,39 @@
-const removeFromArray = function() {
 
-};
+const removeFromArray = function (inputArray, ...args) {
+
+    if (!(inputArray instanceof Array)) {
+        throw new Error('Invalid argument type, expecting array');
+    }
+    if (args.length === 0 || inputArray.length === 0) {
+        return inputArray;
+    }
+    const retArr = [];
+    for (let i = 0; i < inputArray.length; i++) {
+        console.log(inputArray.length);
+        const element = inputArray[i];
+
+        // whether we need to include this value in return array 
+        let includeElementInReturnArray = true; 
+        for (let j = 0; j < args.length; j++) {
+            console.log(args[j] ,  element);
+            if (args[j] === element) {
+                includeElementInReturnArray = false;
+                // inputArray.splice(i, 1);
+            }
+        }
+        if(includeElementInReturnArray){
+            retArr.push(element);
+        }
+
+    }
+    return retArr;
+}
+
+
+
+
+// example  [1,2,3,4],7,4,2,54,76,77, -1 output [1,3]
+ 
 
 // Do not edit below this line
 module.exports = removeFromArray;
