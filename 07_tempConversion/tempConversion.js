7438f4792c41e1de2fe0e26b37f6048205022994
BellaTovm <tovmasyanmd@gmail.com>
2022-09-15 14:08:13 +0100

    temConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const ftoc = function() {
+const ftoc = function(far) {
+let cels;
+cels= (far-32)* (5/9);
+cels= Number(cels.toFixed(1));
+return cels;
+};
+// console.log(ftoc(32))
+
+const ctof = function(cels) {
+  let far;
+  far =  (cels * 9/5) + 32 ;
+  far = Number(far.toFixed(1));
+  return far;
 
 };
-
-const ctof = function() {
-
-};
-
+// console.log(ctof(54));
 // Do not edit below this line
 module.exports = {
   ftoc,
