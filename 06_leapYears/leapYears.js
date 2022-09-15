def02eac9cda82ec03141f30aa5a8b5d6452ab3a
BellaTovm <tovmasyanmd@gmail.com>
2022-09-15 11:48:17 +0100

    LeapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
+const leapYears = function (years) {
+    if (years %4 === 0 && !(years % 100 === 0 && years % 400 !== 0)){
+        return true;
+    } else {
+        return false;
+    }
 
-};
+    // if (years % 4 !== 0) {
+    //     return false;
+    // }
 
+    // if (years % 100 === 0 && years % 400 !== 0) {
+    //     return false;
+    // }
+    // return true;
+
+}
+// console.log(leapYears(1996))
 // Do not edit below this line
 module.exports = leapYears;
