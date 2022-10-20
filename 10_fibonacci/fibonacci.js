23f72564ffceb4b134147af641028b704502ab98
BellaTovm <tovmasyanmd@gmail.com>
2022-10-20 14:55:14 +0100

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let arr =[1,1];
+    if (n<0){
+        return "OOPS";
+    }
+  
+    for(let i=2; i<=n; i++){
+        
+     arr[i] = arr[i-1] + arr[i-2] ;
+    }
+return arr[n-1];
+
+
+
+/*const initialValue = 0;
+const sumWithInitial = arr.reduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  initialValue
+);*/
 
 };
 
