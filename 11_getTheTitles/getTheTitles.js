6be70ec2fd598b296813fcee47c0d560b1fc736c
BellaTovm <tovmasyanmd@gmail.com>
2022-10-21 11:07:52 +0100

    titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,13 @@
-const getTheTitles = function() {
 
+const getTheTitles = function(books) {
+    let newArray=[];
+  for (let index in books){
+    //for (let index = 0; index< books.length; index++){
+   newArray.push(books[index].title);
+  };
+ return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
